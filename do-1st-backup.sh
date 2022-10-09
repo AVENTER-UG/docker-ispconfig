84c8728b975454e05a0cd057438fae3eadafc731
FalkoInc <50336107+FalkoInc@users.noreply.github.com>
2022-10-09 12:20:45 -0700

    updated panel composer example and 1st backup

--- do-1st-backup.sh
+++ do-1st-backup.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 
-cp -r --parents /var/lib/amavis/ /var/backup/
-cp -r --parents /etc/amavis/ /var/backup/
-cp -r --parents /etc/letsencrypt/ /var/backup/
-cp -r --parents /etc/apache2/sites-available/ /var/backup/
-cp -r --parents /etc/apache2/sites-enabled/ /var/backup/
-cp -r --parents /usr/local/ispconfig/ /var/backup/
-cp -r --parents /etc/cron.d/ /var/backup/
-cp -r --parents /etc/bind/ /var/backup/
-cp -r --parents /var/vmail/ /var/backup/
-cp -r --parents /var/www/ /var/backup/
+cp -r --parents /var/lib/amavis/ /data/
+cp -r --parents /etc/amavis/ /data/
+cp -r --parents /etc/letsencrypt/ /data/
+cp -r --parents /etc/apache2/sites-available/ /data/
+cp -r --parents /etc/apache2/sites-enabled/ /data/
+cp -r --parents /usr/local/ispconfig/ /data/
+cp -r --parents /etc/cron.d/ /data/
+cp -r --parents /etc/bind/ /data/
+cp -r --parents /var/vmail/ /data/
+cp -r --parents /var/www/ /data/
 echo "Backup Completed"
